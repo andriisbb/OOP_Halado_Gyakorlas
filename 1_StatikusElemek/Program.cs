e6b3b38094b4877d63128f33f7d454f1ef453c45
andriisbb <boldog.andras.2023d@telekimezotur.hu>
2026-09-24 10:47:53 +0200

    1. projekt főprogram

--- 1_StatikusElemek/Program.cs
+++ 1_StatikusElemek/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using _1_StatikusElemek;
+
+Console.WriteLine($"100 Eur to HUF: {ValutaValto.EurToHuf(100)}");
+Console.WriteLine($"250 Usd to HUF: {ValutaValto.UsdToHuf(250)}");
+Console.WriteLine($"Total conversions: {ValutaValto.GetOsszesAtvaltas()}");
---
+++ 1_StatikusElemek/ValutaValto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_StatikusElemek
+{
+    public static class ValutaValto
+    {
+        public const double EUR_HUF = 395.5;
+        public const double USD_HUF = 360;
+        private static int osszesAtvaltas = 0;
+        public static double EurToHuf(double eur)
+        {
+            osszesAtvaltas++;
+            return eur * EUR_HUF;
+        }
+        public static double UsdToHuf(double usd)
+        {
+            osszesAtvaltas++;
+            return usd * USD_HUF;
+        }
+        public static int GetOsszesAtvaltas()
+        {
+            return osszesAtvaltas;
+        }
+
+    }
+}
