21e9686f20f3ae0dec868ef8e91f09a6d81748b6
andriisbb <boldog.andras.2023d@telekimezotur.hu>
2026-09-25 10:43:57 +0200

    2.feladat hitel

---
+++ 02_AdatvedelemEsKonstruktorok/Program.cs
@@ -0,0 +1,10 @@
+﻿using _02_AdatvedelemEsKonstruktorok;
+
+Bankszamla Anna = new Bankszamla("100");
+Bankszamla Bela = new Bankszamla("200", 100000, 50000);
+
+Anna.Befizetes(100000);
+Bela.Kivetel(20000);
+
+Console.WriteLine($"Anna számlája: {Anna.Szamlaszam}, egyenleg: {Anna.Egyenleg}, hitelkeret: {Anna.HitelKeret}");
+Console.WriteLine($"Béla számlája: {Bela.Szamlaszam}, egyenleg: {Bela.Egyenleg}, hitelkeret: {Bela.HitelKeret}");
